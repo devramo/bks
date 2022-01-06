15c7afcc97ef4eeebe126b48321f3f9916ceb15c
devramo <omar.djelassi@gmail.com>
2022-01-06 22:43:29 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>Assiette au beurre</h1>
+    <ul>
+      <li><a href="L'Assiette_au_beurre__1901_01.pdf">L'Assiette_au_beurre__1901_01.pdf</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
